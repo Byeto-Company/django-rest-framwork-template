ea61c5598e73839ce2329993fbaa927dcec6f628
nima <nimiologyy@gmail.com>
2024-11-10 12:26:55 +0330

    djoser

--- core/settings.py
+++ core/settings.py
@@ -1,5 +1,5 @@
 from dotenv import load_dotenv
-from http.cookiejar import debug
+# from http.cookiejar import debug
 from pathlib import Path
 from datetime import timedelta
 import os
@@ -7,18 +7,16 @@ from django.templatetags.static import static
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 
-
-#TODO CREATE A ENV WITH THIS NAME 
+# TODO CREATE A ENV WITH THIS NAME
 load_dotenv(".env.local")
 
 DOMAIN = os.getenv("DOMAIN")
 API_DOMAIN = os.getenv("API_DOMAIN")
 
-
-#TODO update  telegram bot token
+# TODO update  telegram bot token
 TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 
-#TODO update email bullshit
+# TODO update email bullshit
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = 587
@@ -27,26 +25,20 @@ EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("SECRET_KEY")
 
-
-
 SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = os.getenv("DEBUG")
 if not DEBUG:
     ALLOWED_HOSTS = ['127.0.0.1', 'localhost', DOMAIN, API_DOMAIN]
     CSRF_TRUSTED_ORIGINS = [
-    f"https://{DOMAIN}",
-    f"http://{DOMAIN}",
+        f"https://{DOMAIN}",
+        f"http://{DOMAIN}",
     ]
     CORS_ALLOWED_ORIGINS = [f"https://{API_DOMAIN}", f"http://{API_DOMAIN}",
-                        f"http://{DOMAIN}", f"https://{DOMAIN}",]
+                            f"http://{DOMAIN}", f"https://{DOMAIN}", ]
 else:
     CORS_ALLOW_ALL_ORIGINS = True
 
-
-
-
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Application definition
 
@@ -59,7 +51,7 @@ INSTALLED_APPS = [
     "unfold.contrib.import_export",
     "unfold.contrib.guardian",
     "unfold.contrib.simple_history",
-    #django 
+    # django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -71,6 +63,11 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_spectacular',
     'django_cleanup.apps.CleanupConfig',
+    'django_filters',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+    'rest_framework.authtoken',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -105,8 +102,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-
-
 if DEBUG:
     DATABASES = {
         'default': {
@@ -115,19 +110,17 @@ if DEBUG:
         }
     }
 else:
-    #database postgres
+    # database postgres
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.getenv("DB_NAME"),
             'USER': os.getenv("DB_USER"),
             'PASSWORD': os.getenv("DB_PASSWORD"),
-            'HOST': os.getenv("DB_HOST"), 
+            'HOST': os.getenv("DB_HOST"),
             'PORT': os.getenv("DB_PORT"),
         }
     }
-
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -144,9 +137,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -154,8 +144,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -165,11 +153,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 
 # AUTH_USER_MODEL = 'accounts.User'
 
@@ -192,7 +176,6 @@ REST_FRAMEWORK = {
     # ],
 }
 
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=os.getenv("ACCESS_TOKEN_LIFETIME")),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=os.getenv("REFRESH_TOKEN_LIFETIME")),
@@ -200,18 +183,13 @@ SIMPLE_JWT = {
     'BLACKLIST_AFTER_ROTATION': True,
 }
 
-
 SPECTACULAR_SETTINGS = {
     'TITLE': os.getenv("SITE_TITLE"),
     'DESCRIPTION': os.getenv("SITE_TITLE"),
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
-    'COMPONENT_SPLIT_REQUEST': True 
+    'COMPONENT_SPLIT_REQUEST': True
 }
-
-
-
-
 
 UNFOLD = {
     "SITE_TITLE": os.getenv("SITE_TITLE"),
@@ -251,13 +229,9 @@ UNFOLD = {
 }
 
 
-
-
 def environment_callback(request):
     return ["Development", "warning"]
 
 
 def badge_callback(request):
     return 3
-
-
--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,12 @@ from django.conf import settings
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 urlpatterns = [
+
+    # djoser
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+
+
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('admin/', admin.site.urls),
